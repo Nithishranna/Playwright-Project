f1ea403a059a1cc21d9f81dd4a00d487b55e226b
Nithish Ranna <nithishselvam007@gmail.com>
2026-06-15 14:38:40 +0530

    added CI/CD workflow

--- tests/test_google.py
+++ tests/test_google.py
@@ -1,5 +1,12 @@
-from playwright.sync_api import Page
+from pages.google_page import GooglePage
+from utils.json_reader import read_json
 
-def test_google_title(page: Page):
-    page.goto("https://www.google.com")
-    assert "Google" in page.title()
+
+def test_google_title(page):
+    test_data = read_json("test_data/google_data.json")
+
+    google = GooglePage(page)
+
+    google.open()
+
+    assert test_data["expected_title"] in google.get_title()
